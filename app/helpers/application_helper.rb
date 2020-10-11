b54490a568b9731fad9bc7d516d33d4a156463ea
Zaryab riaz <zaryabriaz@gmail.com>
2020-10-12 00:49:37 +0500

    added everything before making channels

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
 
 def gravatar_url(user)
-  binding.pry
   gravatar_id = Digest::MD5::hexdigest(user.email).downcase
   url = "https://gravatar.com/avatar/#{gravatar_id}.png"
 end
